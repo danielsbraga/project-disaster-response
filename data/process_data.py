295def70b37d2e8cd75f490af1bad69eef1e9d75
DanielSB <sindeauxdaniel@gmail.com>
2024-07-14 20:05:29 -0300

    Update data/process_data.py file with training/ETL Pipeline Preparation.ipynb code

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,57 @@
 import sys
 
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import re
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    # import datsets
+    messages = pd.read_csv(messages_filepath).reset_index()
+    categories = pd.read_csv(categories_filepath).reset_index()
+    # merge datasets
+    keepcol = ['id','index','message', 'original', 'genre', 'categories']
+    df = pd.merge(messages,categories,on='index',suffixes=('', '_1'))[keepcol]\
+        .set_index('index')
+    return df
 
+def clean_data(df):    
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
+    
+    # extract a list of new column names for categories.
+    row = categories.iloc[0]
+    category_colnames = [re.sub(r'-\d+', '', i) for i in row]
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x[-1])
 
-def clean_data(df):
-    pass
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        
+    # drop the original categories column from `df`
+    df.drop('categories',axis=1,inplace=True)
 
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+    
+    # drop duplicates
+    if df.duplicated().sum() > 0:
+        df = df.drop_duplicates()
+
+    #remove possible erros in related column
+    df = df[df['related'] != 2]
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    # convert DataFrame to SQL DataSet 
+    engine = create_engine(f'sqlite:///{database_filename}')
+    # If there already is a message table, it will be removed
+    df.to_sql('messages', engine, index=False, if_exists='replace')
 
 
 def main():
